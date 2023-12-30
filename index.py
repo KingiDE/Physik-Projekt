6b9dc741b83a941613a1968b25525a59a6467843
KingiDE <lodesleon@gmail.com>
2023-12-30 14:11:52 +0100

    add arrows

--- index.py
+++ index.py
@@ -22,17 +22,22 @@ while running:
 
     font = pygame.font.Font(None, 36)
 
-    # origin: 75 / 825
+    origin_x = 75
+    origin_y = 825
+
     pygame.draw.rect(screen, '#f5f5f5', (73, 75, 5, 750)) # Y Axis Rectangle
-    pygame.draw.rect(screen, '#f5f5f5', (75, 823, 1250, 5)) # X Axis Rectangle
+    pygame.draw.rect(screen, '#f5f5f5', (origin_x, 823, 1250, 5)) # X Axis Rectangle
+
+    pygame.draw.polygon(screen, '#f5f5f5', ((65, 75), (75, 60), (85, 75))) # Y Axis Arrow
+    pygame.draw.polygon(screen, '#f5f5f5', ((1325, 815), (1340, 825), (1325, 835))) # X Axis Arrow
 
     for value in [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]:
-        pygame.draw.rect(screen, '#f5f5f5', (75 + value * 20, 825, 1, 20))
-        screen.blit(font.render(str(value), True, '#f5f5f5'), (75 + value * 20 - 10, 860))
+        pygame.draw.rect(screen, '#f5f5f5', (origin_x + value * 20, origin_y, 1, 20))
+        screen.blit(font.render(str(value), True, '#f5f5f5'), (origin_x + value * 20 - 10, 860))
     
     for value in [0, 10, 20, 30, 40, 50, 60, 70, 80, 90]:
-        pygame.draw.rect(screen, '#f5f5f5', (55, 825 - (value * 8), 20, 1))
-        screen.blit(font.render(str(value), True, '#f5f5f5'), (20, 825 - (value * 8) - 10))
+        pygame.draw.rect(screen, '#f5f5f5', (origin_x - 20, origin_y - (value * 8), 20, 1))
+        screen.blit(font.render(str(value), True, '#f5f5f5'), (20, origin_y - (value * 8) - 10))
 
     pygame.display.flip() # put your work on screen
 
